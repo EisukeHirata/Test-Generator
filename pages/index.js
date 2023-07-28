0128d134956d19b419eb120e884d5fe9878701ab
Eisuke Hirata <hirata.busirug@gmail.com>
2023-07-28 14:38:10 +0900

    update interface

--- pages/index.js
+++ pages/index.js
@@ -274,7 +274,7 @@ const Home = () => {
             <div className="output">
               <div className="output-header-container">
                 <div className="output-header">
-                  <h3>Output</h3>
+                  <h3>Your Test</h3>
                 </div>
               </div>
               <div className="my-4">
